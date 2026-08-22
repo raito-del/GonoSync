3628333e6e13a93dc16237cdd0813e667b766a77
raito-del <wazed7820@gmail.com>
2026-08-22 15:23:15 +0600

    Add Firebase Google Services plugin
    Added Firebase Google Services plugin to the build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    // ফায়ারবেস গুগল সার্ভিসেস প্লাগইন যুক্ত করা হলো:
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
-
